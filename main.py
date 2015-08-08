2b8c97e21cdcb128e8e8a8ed735dc1d81dc05cb1
Mio <miostarkid@gmail.com>
2015-08-08 16:02:18 +0200

    Bugfixes and the right main.py.

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from core.MultiThreader import MultiThreader
 from core.MassdropBot import MassdropBot
 
 if __name__ == "__main__":
     mb = MassdropBot()
-    thr = MultiThreader()
-    thr.go(thr.repeater, thr.repeater, thr.repeater)
-    try:
-        thr.join_threads()
-    except KeyboardInterrupt:
-        print("Exiting.")
