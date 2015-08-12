93c4064cad583e489745d98ce2a0694ef290c4b1
Mio <miostarkid@gmail.com>
2015-08-12 21:21:42 +0200

    Thanks massdrop for your awesome website change which populates the entire website from Javascript, and thanks for including bound functions inside a json which are seriously fucking hard to parse. Witty witty.

--- core/filtering/Massdrop.py
+++ core/filtering/Massdrop.py
@@ -4,6 +4,8 @@ from os.path import dirname
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.error import HTTPError
+from datetime import datetime
+from json import loads as json_loads
 import re
 from json import loads
 from praw.objects import Comment
@@ -22,36 +24,44 @@ class Massdrop(Base):
         self.session, self.oauth = self.factory_reddit(config_file=dirname(__file__)+"/../config/"+self.OAUTH_FILENAME)
         self.responses = MassdropText(dirname(__file__) + "/../config/bot_config.ini")
 
+        self.RE_DATASET = re.compile('"DropsStore":({.*?}),"StatsStore"', re.UNICODE)
+
     def execute_comment(self, comment):
         url = self.REGEX.findall(comment.body)
         if url:
-            response = self.generate_response(url)
+            response, time_ends_in = self.generate_response(url)
             if response:
                 self.oauth.refresh()
                 generated = self.session._add_comment(comment.fullname, response)
-                self.database.insert_into_update(generated.name, self.BOT_NAME, 1209600, 43200)
+                if time_ends_in:
+                    self.database.insert_into_update(generated.name, self.BOT_NAME,
+                                                     time_ends_in.seconds + 13*60*60, 43200)
                 return True
         return False
 
     def execute_submission(self, submission):
         url = self.REGEX.findall(submission.selftext)
         if url:
-            response = self.generate_response(url)
+            response, time_ends_in = self.generate_response(url)
             if response:
                 self.oauth.refresh()
                 generated = self.session._add_comment(submission.name, response)
-                self.database.insert_into_update(generated.name, self.BOT_NAME, 1209600, 43200)
+                if time_ends_in:
+                    self.database.insert_into_update(generated.name, self.BOT_NAME,
+                                                     time_ends_in.seconds + 13*60*60, 43200)
                 return True
         return False
 
     def execute_link(self, link_submission):
         link = self.REGEX.findall(link_submission.url)
         if link:
-            response = self.generate_response(link)
+            response, time_ends_in = self.generate_response(link)
             if response:
                 self.oauth.refresh()
                 generated = self.session._add_comment(link_submission.name, response)
-                self.database.insert_into_update(generated.name, self.BOT_NAME, 1209600, 43200)
+                if time_ends_in:
+                    self.database.insert_into_update(generated.name, self.BOT_NAME,
+                                                     time_ends_in.seconds + 13*60*60, 43200)
                 return True
         return False
 
@@ -61,7 +71,7 @@ class Massdrop(Base):
     def update_procedure(self, thing_id, created, lifetime, last_updated, interval):
         self.oauth.refresh()
         comment = self.session.get_info(thing_id=thing_id)
-        if comment.score < -2:
+        if comment.score < -5:
             comment.delete()
             self.database.delete_from_update(thing_id, self.BOT_NAME)
             return
@@ -76,7 +86,7 @@ class Massdrop(Base):
     def execute_textbody(self, string):
         url = self.REGEX.findall(string)
         if url:
-            response = self.generate_response(url)
+            response, time_ends_at = self.generate_response(url)
             print(response)
 
     def generate_response(self, massdrop_links, time_left=None, from_update=False):
@@ -85,6 +95,8 @@ class Massdrop(Base):
         drop_field = []
         textbody = ""
         fixed_urls = 0
+        time_ends_in = None
+        will_update = False
         for url in massdrop_links:
             # if that is already a fixed url, we may ignore it.
             fix_url = url
@@ -94,26 +106,21 @@ class Massdrop(Base):
                 fix_url = fix_url + ('?', '&')['?' in url] + 'mode=guest_open'
                 self.logger.debug('Trying to open {} .'.format(fix_url))
             try:
-                bs = BeautifulSoup(urlopen(fix_url), "html.parser")
-                # There is only one H1 - the product name - which is handy.
-                product_name = bs.find('h1').string
-                # BS4 returns a type error on a find.strings when there is nothing. So we select first, check if there
-                # is something and if so, we join that (since they're split like that: $ | 49 | .99 )
-                current_price = ""
-                current_price_selector = bs.find('strong', attrs={'class': 'current-price'})
-                if current_price_selector:
-                    current_price = ''.join(current_price_selector.strings)
-                # Here is a story: Massdrop serves their old drops with full pricing on their webpage - even
-                # if the drop isn't running. Otherwise this is deep selection of data they store in a div. -shrug-
-                prices = ""
-                pricedrops = bs.find('div', attrs={'class': 'threshold-bars'})
-                if current_price:
-                    prices = loads(pricedrops.attrs['data-drop-info'])['prices']
+                content = urlopen(fix_url).read().decode('utf-8')
+                drop_data = self.RE_DATASET.search(content).groups()[0]
+                drop_data = json_loads(drop_data)
+                product_name = drop_data['drop']['name']
+                if drop_data['drop']['statusCode'] == 1:
+                    current_price = "${:.2f}".format(drop_data['drop']['currentPrice'])
+                    prices = [x['price'] for x in drop_data['drop']['steps']]
                     prices = massdrop_pricer(current_price, prices)
-                    # time represented as string like: 4 DAYS LEFT, hence the lower().
-                    time_left = " / " + bs.find('span', attrs={'class': 'med0text item-time'}).string.lower()
+                    time_ends_at = datetime.strptime(drop_data['drop']['endAt'], '%Y-%m-%dT%H:%M:%S.%fZ')
+                    time_ends_in = time_ends_at - datetime.utcnow()
+                    time_left = " / " + self.time_formatter(time_ends_in)
+                    will_update = True
                 else:
                     time_left = "drop has ended"
+                    current_price, prices = "", ""
                 drop_field.append({"title": product_name, 'current_price': current_price, 'prices': prices,
                                    "time_left": time_left, 'fix_url': fix_url})
                 fixed_urls += 1
@@ -130,9 +137,18 @@ class Massdrop(Base):
         for item in drop_field:
             textbody += self.responses.product_binding.format(**item)
 
+        update_string = ['', ' ^| ^This ^comment ^updates ^every ^12 ^hours.'][will_update]
+
         textbody = self.responses.intro_drop.format(products=('product', 'products')[fixed_urls > 1]) + \
-                   textbody + self.responses.outro_drop
-        return textbody.replace('\\n', '\n')
+                   textbody + self.responses.outro_drop.format(update=update_string)
+
+        return textbody.replace('\\n', '\n'), time_ends_in
+
+    @staticmethod
+    def time_formatter(time_left):
+        if time_left.days > 0:
+            return "{} days left".format(time_left.days)
+        return "{] hours left".format(time_left.seconds//3600)
 
 
 class MassdropText:
@@ -176,10 +192,12 @@ def init(database):
     return Massdrop(database)
 
 if __name__ == "__main__":
-    mt = MassdropText("..\config\MassdropResponses.ini")
+    from core.DatabaseProvider import DatabaseProvider
+    mt = MassdropText("../config/bot_config.ini")
     print(mt)
-    md = Massdrop()
+    db = DatabaseProvider()
+    md = Massdrop(db)
 
-    # print(md.execute_textbody('https://www.massdrop.com/buy/goboof-alfa'))
+    print(md.execute_textbody('https://www.massdrop.com/buy/goboof-alfa https://www.massdrop.com/buy/arizer-extremeq'))
 
-    print(md.update_procedure("t1_ctv1673", 0, 0, 0, 0))
+    # print(md.update_procedure("t1_ctv1673", 0, 0, 0, 0))
