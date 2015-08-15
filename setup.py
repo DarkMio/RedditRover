15c923f675c2e33484fa8e4c0a179aacae34f855
Mio <miostarkid@gmail.com>
2015-08-15 10:09:10 +0200

    ᕙ(░ಥ╭͜ʖ╮ಥ░)━☆ﾟ.*･｡ﾟ MASSDROPBOT ﾟ｡･*.ﾟ☆━(░ಥ╭͜ʖ╮ಥ░)ᕗ

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='MassdropBot',
@@ -9,5 +9,19 @@ setup(
     author='Martin Zier',
     author_email='miostarkid@gmail.com',
     description='MassdropBot is a framework to host multiple bots that trigger on comments or submissions on reddit.',
-    install_requires=['praw', 'praw-OAuth2Util']
+    install_requires=['praw>=3.1.0', 'praw-OAuth2Util>=0.2.2'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ]
 )
