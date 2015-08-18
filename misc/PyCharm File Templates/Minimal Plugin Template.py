46d7b341a8880d1880f04c84022df1bc4ddbece2
Mio <miostarkid@gmail.com>
2015-08-18 10:17:28 +0200

    PyCharm File-Templates for fast plugin generation.

---
+++ misc/PyCharm File Templates/Minimal Plugin Template.py
@@ -0,0 +1,34 @@
+from core.BaseClass import Base
+
+
+class ${NAME}(Base):
+
+    def __init__(self, database):
+        super().__init__(database)
+        self.BOT_NAME = '${NAME}'
+        self.DESCRIPTION = '${Description}'
+
+    def execute_comment(self, comment):
+        pass
+
+    def execute_titlepost(self, title_only):
+        pass
+
+    def execute_link(self, link_submission):
+        pass
+
+    def execute_submission(self, submission):
+        pass
+
+    def update_procedure(self, thing_id, created, lifetime, last_updated, interval):
+        pass
+
+    def on_new_message(self, message):
+        self.standard_ban_procedure()
+        pass
+
+
+
+def init(database):
+    """Init Call from module importer to return only the object itself, rather than the module."""
+    return ${NAME}(database)
