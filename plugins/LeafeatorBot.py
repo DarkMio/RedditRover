6b17814ebf604a0eba74171813d2df71b9e68917
Mio <miostarkid@gmail.com>
2015-08-26 16:30:15 +0200

    Little hack so Leafeator-Bot only comments once per thread.

--- plugins/LeafeatorBot.py
+++ plugins/LeafeatorBot.py
@@ -20,7 +20,8 @@ class LeafeatorBot(Base):
             re.IGNORECASE)
 
     def execute_comment(self, comment):
-        return self.general_action(comment.body, comment.fullname, comment.subreddit.display_name, comment.author.name)
+        return self.general_action(comment.body, comment.fullname, comment.subreddit.display_name, comment.author.name,
+                                   comment.submission.name)
 
     def execute_titlepost(self, title_only):
         pass
@@ -30,20 +31,23 @@ class LeafeatorBot(Base):
 
     def execute_submission(self, submission):
         return self.general_action(submission.selftext, submission.name, submission.subreddit.display_name,
-                                   submission.author.name)
+                                   submission.author.name, submission.name)
 
     def update_procedure(self, thing_id, created, lifetime, last_updated, interval):
         pass
 
-    def general_action(self, body, thing_id, subreddit, username):
+    def general_action(self, body, thing_id, subreddit, username, thread_id):
         if not subreddit.lower() in self.APPROVE or 'leafeator' in username.lower():
             # filtering out all other stuff
+            return False
+        if self.database.retrieve_thing(thread_id, self.BOT_NAME):
             return False
 
         result = self.REGEX.search(body)
         if result:
             self.oauth.refresh()
             self.session._add_comment(thing_id, self.RESPONSE)
+            self.database.insert_into_storage(thread_id, self.BOT_NAME)
             return True
         return False
 
