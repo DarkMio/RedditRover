4dc35cc79c6ae1653c785208e03ca1eee0b0b607
Mio <miostarkid@gmail.com>
2015-08-30 12:26:48 +0200

    (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ Raise your spaghetti-code!

--- misc/ConfigWizard.py
+++ misc/ConfigWizard.py
@@ -2,7 +2,11 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter.ttk import Combobox
-from configparser import ConfigParser
+from configparser import ConfigParser, NoOptionError
+
+
+"""This ConfigWizard is for editing and creating bot configs quickly. It looks ugly and it could certainly written
+   way, way better, but accept it as what it is. A little ugly piece of code generating a nice little UI."""
 
 
 class ConfigSelector(Frame):
@@ -24,19 +28,39 @@ class ConfigSelector(Frame):
         self.config_parser = cp = ConfigParser()
         cp.read(config_path)
         self.sections = cp.sections()
+        self.config_path = config_path
         self.init_ui('Config Editor', self.sections)
 
     def button_action_click(self):
         list_select = self.listbox_configs.curselection()
         item_select = list_select[0]
         name_select = self.listbox_configs.get(item_select)
-        text_greeting = 'Hallo ' + name_select + '!'
-        self.label_text.config(text=text_greeting)
+
+        def window_destroy():
+            window.destroy()
+            parent.deiconify()
+        parent = self.parent
+        parent.iconify()
+
+        window = Toplevel(self)
+        new_dialog = ConfigDialog(window, self.config_parser, edit=True, section=name_select)
+        window.bind('<Destroy>', lambda x: root.deiconify)
+        window.protocol("WM_DELETE_WINDOW", window_destroy)
 
     def new_dialog(self):
-        window = Tk()
+        def window_destroy():
+            window.destroy()
+            parent.deiconify()
+            populate_list(self.config_parser.sections())
+
+        populate_list = self.populate_list
+        parent = self.parent
+        parent.iconify()
+
+        window = Toplevel(self)
         new_dialog = ConfigDialog(window, self.config_parser)
-        window.mainloop()
+        window.bind('<Destroy>', lambda x: root.deiconify)
+        window.protocol("WM_DELETE_WINDOW", window_destroy)
 
     def init_ui(self, title, sections):
         parent = self.parent
@@ -54,9 +78,10 @@ class ConfigSelector(Frame):
         self.text = StringVar()
 
         self.listbox_configs = Listbox(master=self.frame_box, selectmode='browse', relief=FLAT)
-        for section in sections:
-            self.listbox_configs.insert('end', section)
+        self.populate_list(sections)
         self.listbox_configs.place(x=5, y=5, width=280, height=410)
+        self.listbox_configs.bind('<FocusIn>', self.add_list(self.config_parser.sections()[-1]))
+        self.listbox_configs.bind('<FocusOut>', self.add_list(self.config_parser.sections()[-1]))
 
         self.label_text = Label(master=self.frame_output, bg='white')
         self.label_text.place(x=5, y=5, width=280, height=20)
@@ -65,7 +90,17 @@ class ConfigSelector(Frame):
         self.button_edit.place(x=5, y=5, width=100, height=20)
 
         self.button_new = Button(master=self.frame_action, text='new', command=self.new_dialog, relief=FLAT)
-        self.button_new.place(x=290-100-5, y=5, width=100, height=20)
+        self.button_new.place(x=290 - 100 - 5, y=5, width=100, height=20)
+
+    def populate_list(self, sections):
+        self.listbox_configs.delete(0, 'end')
+        for section in sections:
+            self.listbox_configs.insert('end', section)
+
+    def add_list(self, section):
+        self.config_parser.read(self.config_path)
+        if section not in self.listbox_configs.get(0, 'end'):
+            self.listbox_configs.insert('end', section)
 
 
 class ConfigDialog(Frame):
@@ -82,19 +117,30 @@ class ConfigDialog(Frame):
     username_field = None
     oauth_field = None
     button_save = None
+    edit = None
 
     def __init__(self, parent, config_parser, edit=False, section=None):
         Frame.__init__(self, parent, background="white")
         self.parent = parent
         self.config_parser = config_parser
         self.section = section
+        self.edit = edit
         if edit:
-            # do data population of dialog and things.
+            self.is_logged_in = config_parser.getboolean(section, 'is_logged_in')
             pass
 
         self.init_ui('Config Creator', self.section)
 
+    def get(self, name):
+        try:
+            return self.config_parser.get(self.section, name)
+        except NoOptionError:
+            return ''
+
     def init_ui(self, title, section):
+        select_section = lambda x, y: x if not self.section else y
+        select = lambda x, y: x if not self.section else self.get(y)
+        select_optional = lambda x, y: x if not self.get(y) else self.get(y)
         parent = self.parent
         parent.title(title)
         if not section:
@@ -108,17 +154,27 @@ class ConfigDialog(Frame):
         self.frame_action = Canvas(master=parent, bg='#FBD975', height=20, width=20)
         self.frame_action.grid(row=7)
 
-        self.section_field = self.build_entry_field('Bot Section Name', self.frame_config, 0, 0, 5)
-        self.var_description = self.build_entry_field('Bot Description', self.frame_config, 1, 0)
+        self.section_field = self.build_entry_field(select_section('Bot Section Name', self.section),
+                                                    self.frame_config, 0, 0, 5)
+        self.var_description = self.build_entry_field(select('Bot Description', 'description'),
+                                                      self.frame_config, 1, 0)
 
-        self.username_field = self.build_entry_field('Username', self.frame_config, 4, 0)
-        self.oauth_field = self.build_entry_field('OAuth Filename', self.frame_config, 5, 0)
+        self.username_field = self.build_entry_field(select_optional('Username', 'username'),
+                                                     self.frame_config, 4, 0)
+        self.oauth_field = self.build_entry_field(select_optional('OAuth Filename', 'oauth_file'),
+                                                  self.frame_config, 5, 0)
+
         self.is_logged_in_frame, self.is_logged_in_var = self.build_combobox_with_label(
-            ('True', 'False'), self.frame_config, 3, 1, [self.username_field, self.oauth_field])
+            self.build_tuple_order(), self.frame_config, 3, 1, [self.username_field, self.oauth_field])
 
         self.button_save = Button(master=self.frame_action, text='save', command=self.save_action_click,
                                   relief=FLAT, bg='white', height=1)
         self.button_save.pack(expand=YES, fill=BOTH)
+
+    def build_tuple_order(self):
+        if self.edit and self.config_parser.get(self.section, 'is_logged_in') == 'True':
+            return 'True', 'False'
+        return 'False', 'True'
 
     @staticmethod
     def build_entry_field(text, master, row, col, pady=2.5):
@@ -149,6 +205,9 @@ class ConfigDialog(Frame):
         cb['values'] = values
         cb.grid(row=0, column=1)
         cb.bind('<<ComboboxSelected>>', lambda x: (var.set(cb.get()), toggle_display()))
+        if var.get() == 'False':
+            for element in toggle_background_elements:
+                element.configure(state='disabled')
         return frame, var
 
     def save_action_click(self):
@@ -159,14 +218,13 @@ class ConfigDialog(Frame):
                 if section not in self.config_parser.sections():
                     self.config_parser.add_section(section)
                 logged_in = self.is_logged_in_var.get()
-                self.config_parser.add_section(section)
                 self.config_parser.set(section, 'description', self.var_description.get())
                 self.config_parser.set(section, 'is_logged_in', logged_in)
                 if logged_in == 'True':
                     self.config_parser.set(section, 'username', self.username_field.get())
                     self.config_parser.set(section, 'oauth_file', self.username_field.get())
-                self.config_parser.write(open('../config/bot_config.ini', 'wb'))
-                self.quit()
+                self.config_parser.write(open('../config/bot_config.ini', 'w'))
+                self.parent.destroy()
 
     def save_check_if_complete(self):
         section, description, is_logged_in = (self.section_field.get(), self.var_description.get(),
@@ -181,12 +239,16 @@ class ConfigDialog(Frame):
         return True
 
 
+def on_closing():
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        root.destroy()
+
 if __name__ == '__main__':
     root = Tk()
     root.geometry('300x500')
     root.resizable(width=FALSE, height=FALSE)
     app = ConfigSelector(root, '../config/bot_config.ini')
+    root.protocol("WM_DELETE_WINDOW", on_closing)
     root.mainloop()
-
 else:
     raise ImportError('This file is supposed to be run by itself, rather than imported.')
