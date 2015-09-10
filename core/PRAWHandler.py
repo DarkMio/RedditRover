ac4590b8c9ae022c1528076bb22ea2b3c95ee2c4
DarkMio <mio@zier.ws>
2015-09-10 08:38:34 +0200

    Experimental new dispatcher.

--- core/PRAWHandler.py
+++ core/PRAWHandler.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from praw.handlers import *
 from logging import getLogger
-from time import time
+from time import time, sleep
 from requests import Session
 
 
@@ -20,38 +20,12 @@ class PRAWHandler(RateLimitHandler):
 PRAWHandler.request = PRAWHandler.rate_limit(PRAWHandler.request)
 
 
-class Dispatcher:
-    def __init__(self):
-        self.tick = 1
-        self.no_auth = {'last_request': time(), 'requests': []}     # A dict with requests and last dispatch time
-        self.oauth = []                                             # A list of RegisteredApp
-
-    def dispatch(self):
-        for app in self.oauth:
-            app.get_request()
-            # send
-            pass
-        if len(self.no_auth) > 0 and self.no_auth['last_request'] < time() - 1:
-            request = self.no_auth['requests'].pop(0)
-            # handle send event
-            pass
-
-    def add_request(self, request, _cache_key=None):
-        if _cache_key:
-            for app in self.oauth:
-                if app.key == _cache_key:
-                    app += request
-        else:
-            self.no_auth['request'].append(request)
-
-
 class RoverHandler:
 
     def __init__(self):
         self.logger = getLogger('hndl')
-        self.tick = 1                                               # 1 second = 1 tick
-        self.no_auth = {'last_request': time(), 'requests': []}     # A dict with requests and last dispatch time
-        self.oauth = []                                             # A list of RegisteredApp
+        self.no_auth = time() - 1                                   # simply the time since the last no_auth was sent
+        self.oauth = {}                                             #
         self.http = Session()
 
     def __del__(self):
@@ -61,34 +35,28 @@ class RoverHandler:
             except Exception:
                 pass
 
-    @staticmethod
-    def dispatcher(function):
-        @wraps(function)
-        def wrapped(cls):
-            if cls.no_auth['last_request'] < time() - 2 and len(cls.no_auth['requests']) > 0: # A call every 2 seconds
-                cls.send_request(*cls.no_auth['requests'].pop(0))
+    def request(self, request, proxies, timeout, verify, **_):
+        bearer = ''
+        if '_cache_key' in _:
+            cache_key = _.get('_cache_key')
+            token_group = cache_key[1]
+            if len(token_group) >= 5:
+                bearer = token_group[4]
+        if bearer:
+            if bearer in self.oauth:
+                time_dispatched = self.dispatch_timer(self.oauth[bearer] + 1)
+                return self.send_request(request, proxies, timeout, verify)
+        else:
+            time_dispatched = self.dispatch_timer(self.no_auth + 2)
+            return self.send_request(request, proxies, timeout, verify)
 
     def send_request(self, request, proxies, timeout, verify):
         self.logger.debug('{:4} {}'.format(request.method, request.url))
         return self.http.send(request, proxies=proxies, timeout=timeout, allow_redirects=False, verify=verify)
 
-class RegisteredApp:
-
-    def __init__(self, unique_key):
-        self.key = unique_key           # A simple string, originally request_token from OAuth
-        self.created = time()           # Time created, so we can delete itself if needed.
-        self.last_request = time() - 1  # Time of the last request, on creation simply default value
-        self.requests = []              # All upcoming requests from oldest -> newest
-
-    def __add__(self, other):
-        self.requests.append(other)
-
-    def get_request(self):
-        if len(self.requests) > 0 and self.last_request > time() - 1:
-            self.last_request = time()
-            return self.requests.pop(0)
-
-    def delete_object(self, lifetime):
-        if lifetime < time() and len(self.requests):
-            del self
-
+    @staticmethod
+    def dispatch_timer(next_possible_dispatch):
+        time_until_dispatch = (next_possible_dispatch) - time()
+        if time_until_dispatch > 0:  # Make sure that we have given it enough time.
+            sleep(time_until_dispatch)
+        return time()
