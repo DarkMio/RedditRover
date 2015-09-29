8dc8c4fdacdcd36e50800499dc62f010ed7fa684
DarkMio <mio@zier.ws>
2015-09-30 00:46:13 +0200

    small updates + missing files

--- util/stats.py
+++ util/stats.py
@@ -200,15 +200,15 @@ class StatisticsFeeder:
         carelist = []
         messages = self.db.get_all_messages()
         message_template = 'You\'ve mailed my bot here: http://reddit.com/message/messages/{msg_id}%0A%0A---%0A%0A'
-        url_template = 'http://reddit.com/message/compose/?to={author}&subject={subject}&body={msg_template}'
-        reply_template = '<a href="{answer_url}"> {body} </a>'
+        url_template = 'http://reddit.com/message/compose/?to={author}&subject={subject}&message={msg_template}'
+        reply_template = '<a href="{answer_url}" target="_blank"> {body} </a>'
         for line in messages:
             msg = message_template.format(msg_id=line[0])
             url = url_template.format(author=line[4], subject=line[3], msg_template=msg)
             reply = reply_template.format(answer_url=url, body=line[5])
-            carelist.append({'id': line[0], 'bot_module': line[1], 'created': line[2], 'title': line[3],
-                             'author': line[4], 'body': reply})
-        with open(self.path + '_data/overview_rows.json', 'w') as f:
+            carelist.append({'id': line[0], 'plugin': line[1], 'time': line[2], 'title': line[3],
+                             'username': line[4], 'body': reply})
+        with open(self.path + '_data/message_rows.json', 'w') as f:
             f.write(json.dumps(carelist))
 
 if __name__ == "__main__":
